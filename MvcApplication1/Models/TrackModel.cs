e0e4713198036476f2d6217f3be2de32b888fb79
Frank Lynch <franklynch67@gmail.com>
2015-01-19 20:40:14 +0000

    refactor

---
+++ MvcApplication1/Models/TrackModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAsp.Models
+{
+    public class TrackModel
+    {
+        public int _id { get; set; }
+        public string _name{ get; set; }
+    }
+}
